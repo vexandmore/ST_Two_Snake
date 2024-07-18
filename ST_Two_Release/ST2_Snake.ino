7e3026a70d7bff193d5af10f317204590ee03844
Marc Scattolin <marcscattolin@gmail.com>
2024-07-18 10:54:48 -0400

    working!

--- ST_Two_Release/ST2_Snake.ino
+++ ST_Two_Release/ST2_Snake.ino
@@ -7,9 +7,11 @@ int8_t direction = 1; // 0, 1, 2, 3 mean North, East, South, West
 uint8_t snakeCoords[140][2] = {0};
 // Where in the array the head and tail are
 uint8_t head = 0, tail = 0;
+// Where the pellet is
+uint8_t pelletRow, pelletColumn;
 
 bool snakeDead(uint8_t nextHeadRow, uint8_t nextHeadCol) {
-  for (int i = tail; i != head; i = (i + 1) % 139) {
+  for (int i = tail; i != ((head + 1) % 140); i = (i + 1) % 140) {
     if (snakeCoords[i][0] == nextHeadRow &&
         snakeCoords[i][1] == nextHeadCol) {
       return true;
@@ -18,15 +20,46 @@ bool snakeDead(uint8_t nextHeadRow, uint8_t nextHeadCol) {
   return false;
 }
 
+bool atPellet(uint8_t nextHeadRow, uint8_t nextHeadCol) {
+  return nextHeadRow == pelletRow && nextHeadCol == pelletColumn;
+}
+
+// Place pellet somewhere the snake isn't
+void placePellet() {
+  while (true) {
+    pelletRow = random(7);
+    pelletColumn = random(19);
+
+    bool pelletIsOnSnake = false;
+    
+    for (int i = tail; i != ((head + 1) % 140); i = (i + 1) % 140) {
+      if (snakeCoords[i][0] == pelletRow &&
+          snakeCoords[i][1] == pelletColumn) {
+        pelletIsOnSnake = true;
+        break; // need to redraw; just exit loop
+      }
+    }
+
+    if (!pelletIsOnSnake) {
+      // exit if we've picked a valid location
+      break;
+    }
+  }
+}
+
 void clearGame() {
   head = 0;
   tail = 0;
+  // Reset array to all 0
   for (int i = 0; i < 140; i++) {
     snakeCoords[i][0] = 0;
     snakeCoords[i][1] = 0;
   }
   snakeCoords[0][0] = 3; // Make start in the middle row
   direction = 1;
+
+  pelletRow = random(7);
+  pelletColumn = random(4, 19);
   
   clearmatrix();
 }
@@ -34,11 +67,14 @@ void clearGame() {
 void drawSnake() {
   clearmatrix();
   
-  for (int i = 0; i <= head; i++) {
+  for (int i = tail; i != ((head + 1) % 140); i = (i + 1) % 140) {
     int row = snakeCoords[i][0];
     int col = snakeCoords[i][1];
     LEDMAT[col] |= 1 << row;
   }
+
+  // Draw pellet
+  LEDMAT[pelletColumn] |= 1 << pelletRow;
 }
 
 
@@ -74,12 +110,6 @@ void snake()
       delay(75); // prevent from moving too fast
       int nextHeadRow = snakeCoords[head][0];
       int nextHeadCol = snakeCoords[head][1];
-      head++;
-      
-      if (head == 139) {
-        SUBSTATE == 99;
-        break;
-      }
       
       if (direction == 0) {
         nextHeadRow--;
@@ -99,12 +129,26 @@ void snake()
       // the snake has run into itself
       if (snakeDead(nextHeadRow, nextHeadCol)) {
         clearGame();
+        break;
       } else {
+        head++;
+        head %= 140;
         snakeCoords[head][0] = nextHeadRow;
         snakeCoords[head][1] = nextHeadCol;
-
-        drawSnake();
       }
+
+      // Check if that puts the snake head on the pellet
+      if (atPellet(nextHeadRow, nextHeadCol)) {
+        // If so, snake grows and re-put pellet
+        placePellet();
+      } else {
+        // If not, snake doesn't grow
+        tail++;
+        tail %= 140;
+      }
+
+      drawSnake();
+
 
       break;
     
